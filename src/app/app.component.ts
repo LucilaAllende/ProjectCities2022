7cedf60242d4aa37dd74fba76f8054d9f7517bb8
LucilaAllende <abigail_lucii@hotmail.com>
2022-09-19 12:09:05 -0300

    Using structural directives ngIf and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name = 'Hola mundo'
+  name!:string;
+  cities = ['Barcelona','Madrid','Lima']
   title = 'projectCities';
   url = 'https://www.enter.co/wp-content/uploads/2020/03/21770-1024x768.jpg'
 }
