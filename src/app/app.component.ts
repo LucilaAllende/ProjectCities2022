ffcb1c94832c1e60b208232a52fecf957e7a20f9
LucilaAllende <abigail_lucii@hotmail.com>
2022-09-19 15:54:01 -0300

    Component life cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
+
   name!:string;
   cities = ['Barcelona','Madrid','Lima']
   title = 'projectCities';
   url = 'https://www.enter.co/wp-content/uploads/2020/03/21770-1024x768.jpg'
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges->',changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
 }
